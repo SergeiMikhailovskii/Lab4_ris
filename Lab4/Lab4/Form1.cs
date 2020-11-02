fff97cb04935d0ed33ca5400d85fc9e92b4431eb
SergeiMikhailovskii <mikhailovskii.s@tut.by>
2020-11-03 00:40:19 +0300

    Added lab4

---
+++ Lab4/Lab4/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Lab4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "lab4DataSet.material". При необходимости она может быть перемещена или удалена.
+            this.materialTableAdapter.Fill(this.lab4DataSet.material);
+
+        }
+    }
+}
